af8b40766e02cf62eae3b543041968a1595709c2
ssloy <spam@skisa.org>
2015-01-21 15:15:01 +0100

    matrix class, cube model

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <vector>
+#include <iostream>
 #include <cmath>
 #include "tgaimage.h"
 #include "model.h"
@@ -37,21 +38,22 @@ int main(int argc, char** argv) {
     if (2==argc) {
         model = new Model(argv[1]);
     } else {
-        model = new Model("obj/african_head.obj");
+        model = new Model("obj/cube.obj");
     }
 
     TGAImage image(width, height, TGAImage::RGB);
     for (int i=0; i<model->nfaces(); i++) {
         std::vector<int> face = model->face(i);
-        for (int j=0; j<3; j++) {
+        for (int j=0; j<(int)face.size(); j++) {
             Vec3f v0 = model->vert(face[j]);
-            Vec3f v1 = model->vert(face[(j+1)%3]);
+            Vec3f v1 = model->vert(face[(j+1)%face.size()]);
             int x0 = (v0.x+1.)*width/2.;
             int y0 = (v0.y+1.)*height/2.;
             int x1 = (v1.x+1.)*width/2.;
             int y1 = (v1.y+1.)*height/2.;
             line(x0, y0, x1, y1, image, white);
         }
+        break;
     }
 
     image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
