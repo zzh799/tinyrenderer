862954cdcb8188f88a04c59fa2978097b19cea74
ssloy <spam@skisa.org>
2015-01-21 16:07:10 +0100

    basic functionality

--- main.cpp
+++ main.cpp
@@ -7,25 +7,28 @@
 
 const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red   = TGAColor(255, 0,   0,   255);
+const TGAColor green = TGAColor(0,   255, 0,   255);
+const TGAColor blue  = TGAColor(0,   0,   255, 255);
+
 Model *model = NULL;
 const int width  = 800;
 const int height = 800;
+const int depth  = 255;
 
-void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
+void line(Vec3f p0, Vec3f p1, TGAImage &image, TGAColor color) {
     bool steep = false;
-    if (std::abs(x0-x1)<std::abs(y0-y1)) {
-        std::swap(x0, y0);
-        std::swap(x1, y1);
+    if (std::abs(p0.x-p1.x)<std::abs(p0.y-p1.y)) {
+        std::swap(p0.x, p0.y);
+        std::swap(p1.x, p1.y);
         steep = true;
     }
-    if (x0>x1) {
-        std::swap(x0, x1);
-        std::swap(y0, y1);
+    if (p0.x>p1.x) {
+        std::swap(p0, p1);
     }
 
-    for (int x=x0; x<=x1; x++) {
-        float t = (x-x0)/(float)(x1-x0);
-        int y = y0*(1.-t) + y1*t;
+    for (int x=p0.x; x<=p1.x; x++) {
+        float t = (x-p0.x)/(float)(p1.x-p0.x);
+        int y = p0.y*(1.-t) + p1.y*t+.5f;
         if (steep) {
             image.set(y, x, color);
         } else {
@@ -33,6 +36,70 @@ void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
         }
     }
 }
+
+Vec3f m2v(Matrix m) {
+    return Vec3f(m[0][0]/m[3][0], m[1][0]/m[3][0], m[2][0]/m[3][0]);
+}
+Matrix v2m(Vec3f v) {
+    Matrix m(4, 1);
+    m[0][0] = v.x;
+    m[1][0] = v.y;
+    m[2][0] = v.z;
+    m[3][0] = 1.f;
+    return m;
+}
+
+Matrix viewport(int x, int y, int w, int h) {
+    Matrix m = Matrix::identity(4);
+    m[0][3] = x+w/2.f;
+    m[1][3] = y+h/2.f;
+    m[2][3] = depth/2.f;
+
+    m[0][0] = w/2.f;
+    m[1][1] = h/2.f;
+    m[2][2] = depth/2.f;
+    return m;
+}
+
+Matrix translation(Vec3f v) {
+    Matrix Tr = Matrix::identity(4);
+    Tr[0][3] = v.x;
+    Tr[1][3] = v.y;
+    Tr[2][3] = v.z;
+    return Tr;
+}
+
+Matrix zoom(float factor) {
+    Matrix Z = Matrix::identity(4);
+    Z[0][0] = Z[1][1] = Z[2][2] = factor;
+    return Z;
+}
+
+Matrix rotation_x(float cosangle, float sinangle) {
+    Matrix R = Matrix::identity(4);
+    R[1][1] = R[2][2] = cosangle;
+    R[1][2] = -sinangle;
+    R[2][1] =  sinangle;
+    return R;
+}
+
+Matrix rotation_y(float cosangle, float sinangle) {
+    Matrix R = Matrix::identity(4);
+    R[0][0] = R[2][2] = cosangle;
+    R[0][2] =  sinangle;
+    R[2][0] = -sinangle;
+    return R;
+}
+
+Matrix rotation_z(float cosangle, float sinangle) {
+    Matrix R = Matrix::identity(4);
+    R[0][0] = R[1][1] = cosangle;
+    R[0][1] = -sinangle;
+    R[1][0] =  sinangle;
+    return R;
+}
+
+
 
 int main(int argc, char** argv) {
     if (2==argc) {
@@ -42,19 +109,38 @@ int main(int argc, char** argv) {
     }
 
     TGAImage image(width, height, TGAImage::RGB);
+    Matrix VP = viewport(width/4, width/4, width/2, height/2);
+
+    { // draw the axes
+        Vec3f x(1.f, 0.f, 0.f), y(0.f, 1.f, 0.f), o(0.f, 0.f, 0.f);
+        o = m2v(VP*v2m(o));
+        x = m2v(VP*v2m(x));
+        y = m2v(VP*v2m(y));
+        line(o, x, image, red);
+        line(o, y, image, green);
+    }
+
+
     for (int i=0; i<model->nfaces(); i++) {
         std::vector<int> face = model->face(i);
         for (int j=0; j<(int)face.size(); j++) {
-            Vec3f v0 = model->vert(face[j]);
-            Vec3f v1 = model->vert(face[(j+1)%face.size()]);
-            int x0 = (v0.x+1.)*width/2.;
-            int y0 = (v0.y+1.)*height/2.;
-            int x1 = (v1.x+1.)*width/2.;
-            int y1 = (v1.y+1.)*height/2.;
-            line(x0, y0, x1, y1, image, white);
+            Vec3f wp0 = model->vert(face[j]);
+            Vec3f wp1 = model->vert(face[(j+1)%face.size()]);
+
+            { // draw the original model
+                Vec3f sp0 = m2v(VP*v2m(wp0));
+                Vec3f sp1 = m2v(VP*v2m(wp1));
+                line(sp0, sp1, image, white);
+            }
+            { // draw the deformed model
+                Matrix T = translation(Vec3f(.33, .5, 0))*rotation_z(cos(10.*M_PI/180.), sin(10.*M_PI/180.));
+                Vec3f sp0 = m2v(VP*T*v2m(wp0));
+                Vec3f sp1 = m2v(VP*T*v2m(wp1));
+                line(sp0, sp1, image, blue);
+            }
         }
-        break;
     }
+
 
     image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
     image.write_tga_file("output.tga");
